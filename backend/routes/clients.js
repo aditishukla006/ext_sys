c4bab8f59f5e7f7a36d2e55ca4c125983a1f87ab
Aditi Shukla <shuklaaditi02004@gmail.com>
2026-02-26 17:05:54 +0530

    login update 1.2

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -2,7 +2,7 @@
 const router = require("express").Router();
 const Client = require("../models/client");
 const crypto = require("crypto");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 /* ===============================
    CREATE / GET CLIENT
 ================================ */
